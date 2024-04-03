107e8f5dc34266078ca1fd73ecbd462db8a6a0c3
DandyOoh <57991642+DandyOoh@users.noreply.github.com>
2024-04-03 15:07:28 +0100

    Updates to phases of NHP with all sites included now.

--- map_simple.R
+++ map_simple.R
@@ -13,11 +13,20 @@ icb_map <- as(icb_map, "Spatial")
 
 #### set up hospital locations ####
 
-phase <- c(1,1,1,1,1,1,2,2,2,3,2,2)
-hospital_map_code <- c(1,2,3,4,5,6,7,8,9,10,11,12)
-hospital_name <- c('Airedale','Frimley','Hinchinbrooke','James Paget','Kings Lynn','Leighton','Watford','Whipps Cross','Hillingdon','North Manchester','PAH (Harlow)','Hampshire')
-label_pos <- c('top','top','top','top','top','top','left','right','bottom','top','top','top')
-postcode <- c('BD20 6TD','GU16 7UJ','PE29 6NT','NR31 6LA','PE30 4ET','CW1 4QJ','WD18 0HB','E11 1NR','UB8 3NN','M8 5RB','CM20 1QX','SO22 5DG')
+phase <- c(1,1,1,1,1,1,2,2,2,3,2,2,4,4,4,4,4,5,5,5,5,5,6,6,6,6,6,6)
+hospital_map_code <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28)
+hospital_name <- c('Airedale','Frimley','Hinchinbrooke','James Paget','Kings Lynn','Leighton','Watford','Whipps Cross',
+                   'Hillingdon','North Manchester','PAH (Harlow)','Hampshire','East Sussex','Royal Lancaster',
+                   'North Devon','Royal Berkshire','Royal Preston','Imperial Charing Cross','Hammersmith','Nottingham',
+                   'Imperial St Marys','West Suffolk','Epsom','Kettering','Leicester','Hampshire','Somerset','Torbay')
+label_pos <- c('top','top','top','top','top','top','left','right',
+               'bottom','right','top','top','top','top',
+               'top','left','left','bottom','top','top',
+               'right','top','bottom','bottom','top','top','top','top')
+postcode <- c('BD20 6TD','GU16 7UJ','PE29 6NT','NR31 6LA','PE30 4ET','CW1 4QJ','WD18 0HB','E11 1NR',
+              'UB8 3NN','M8 5RB','CM20 1QX','SO22 5DG','TN37 7RD','LA1 4RP',
+              'EX31 4JB','RG1 5AN','PR2 9HT','W6 8RF','W12 0HS','NG5 1PB',
+              'W2 1NY','IP33 2QZ','KT18 7EG','NN16 8UZ','LE1 5WW','SO22 5DG','TA1 3ES','TQ2 7AA')
 
 nhp_list_1 <- data.frame(phase, hospital_map_code, hospital_name, label_pos, postcode)
 
@@ -43,6 +52,8 @@ nhp_list_1 <- nhp_list_1 |>
 
 #### generate and print map - labels overlapping ####
 
+phasenum <- 3
+
 ## may need to filter on phases for different stages of NHP project...
 
 map <- leaflet(data = icb_map,
@@ -51,8 +62,8 @@ map <- leaflet(data = icb_map,
   addProviderTiles(providers$CartoDB) |> 
   ##addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
               ##opacity = 0.7, fillOpacity = 0) |>
-  addCircleMarkers(data = nhp_list_1 |> filter(phase == 2), lng = ~long, lat = ~lat, color = "blue") |> 
-  addLabelOnlyMarkers(data = nhp_list_1 |> filter(phase == 2), lng=~long, lat=~lat, label =~hospital_name, 
+  addCircleMarkers(data = nhp_list_1 |> filter(phase >= phasenum), lng = ~long, lat = ~lat, color = "blue") |> 
+  addLabelOnlyMarkers(data = nhp_list_1 |> filter(phase >= phasenum), lng=~long, lat=~lat, label =~hospital_name, 
                       labelOptions = labelOptions(noHide = T, direction = "top", textOnly = T, textsize = "12px",
                                                   style = list("color" = "red", "font-weight" = "bold")))
 
@@ -60,10 +71,10 @@ map
 
 #### generate and print map - fixed labels overlapping (fudge!) ####
 
-nhp_list_1_left <- nhp_list_1 |> filter(label_pos == "left", phase == 2)
-nhp_list_1_right <- nhp_list_1 |> filter(label_pos == "right", phase == 2)
-nhp_list_1_top <- nhp_list_1 |> filter(label_pos == "top", phase == 2)
-nhp_list_1_bottom <- nhp_list_1 |> filter(label_pos == "bottom", phase == 2)
+nhp_list_1_left <- nhp_list_1 |> filter(label_pos == "left", phase >= phasenum)
+nhp_list_1_right <- nhp_list_1 |> filter(label_pos == "right", phase >= phasenum)
+nhp_list_1_top <- nhp_list_1 |> filter(label_pos == "top", phase >= phasenum)
+nhp_list_1_bottom <- nhp_list_1 |> filter(label_pos == "bottom", phase >= phasenum)
 
 map_2 <- leaflet(data = icb_map,
                options = leafletOptions(zoomControl = FALSE)) |> 
@@ -71,17 +82,17 @@ map_2 <- leaflet(data = icb_map,
   addProviderTiles(providers$CartoDB) |> 
   ##addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
   ##opacity = 0.7, fillOpacity = 0) |>
-  addCircleMarkers(data = nhp_list_1 |> filter(phase == 2), lng = ~long, lat = ~lat, color = "blue") |> 
-  addLabelOnlyMarkers(data = nhp_list_1_left |> filter(phase == 2), lng=~long, lat=~lat, label =~hospital_name, 
+  addCircleMarkers(data = nhp_list_1 |> filter(phase >= phasenum), lng = ~long, lat = ~lat, color = "blue") |> 
+  addLabelOnlyMarkers(data = nhp_list_1_left |> filter(phase >= phasenum), lng=~long, lat=~lat, label =~hospital_name, 
                       labelOptions = labelOptions(noHide = T, direction = "left", offset = c(-15, 0), textOnly = T, textsize = "12px",
                                                   style = list("color" = "red", "font-weight" = "bold"))) |> 
-  addLabelOnlyMarkers(data = nhp_list_1_right |> filter(phase == 2), lng=~long, lat=~lat, label =~hospital_name, 
+  addLabelOnlyMarkers(data = nhp_list_1_right |> filter(phase >= phasenum), lng=~long, lat=~lat, label =~hospital_name, 
                       labelOptions = labelOptions(noHide = T, direction = "right", offset = c(15, 0), textOnly = T, textsize = "12px",
                                                   style = list("color" = "red", "font-weight" = "bold"))) |> 
-  addLabelOnlyMarkers(data = nhp_list_1_top |> filter(phase == 2), lng=~long, lat=~lat, label =~hospital_name, 
+  addLabelOnlyMarkers(data = nhp_list_1_top |> filter(phase >= phasenum), lng=~long, lat=~lat, label =~hospital_name, 
                       labelOptions = labelOptions(noHide = T, direction = "top", textOnly = T, textsize = "12px",
                                                   style = list("color" = "red", "font-weight" = "bold"))) |> 
-  addLabelOnlyMarkers(data = nhp_list_1_bottom |> filter(phase == 2), lng=~long, lat=~lat, label =~hospital_name, 
+  addLabelOnlyMarkers(data = nhp_list_1_bottom |> filter(phase >= phasenum), lng=~long, lat=~lat, label =~hospital_name, 
                       labelOptions = labelOptions(noHide = T, direction = "bottom", textOnly = T, textsize = "12px",
                                                   style = list("color" = "red", "font-weight" = "bold")))
 
